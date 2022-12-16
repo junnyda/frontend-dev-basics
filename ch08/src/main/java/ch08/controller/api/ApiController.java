4f2a2fe5e94bc0556358e7eb454e4abfeb7de067
junnyda <junnyda1004@gmail.com>
2022-12-16 17:57:12 +0900

    ajax#01

--- ch08/src/main/java/ch08/controller/api/ApiController.java
+++ ch08/src/main/java/ch08/controller/api/ApiController.java
@@ -4,8 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import ch08.vo.UserVo;
 
 @Controller
 @RequestMapping("/api")
@@ -21,4 +25,20 @@ public class ApiController {
 		
 		return map; 
 	}
+	
+	@ResponseBody
+	@RequestMapping(value="/02", method=RequestMethod.POST)
+	public Object ajax02(UserVo vo) {
+		vo.setNo(10L);
+		return vo; 
+	}
+
+	@ResponseBody
+	@RequestMapping(value="/03", method=RequestMethod.POST)
+	public Object ajax03(@RequestBody UserVo vo) {
+		System.out.println(vo);
+		vo.setNo(10L);
+		return vo; 
+	}
+
 }
